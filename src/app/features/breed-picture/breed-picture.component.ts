05a313d704c69369a95bb0fe282f9f1fc38c07fe
Luis Enrique Vega Martinez <luisevegam.91@gmail.com>
2025-04-26 16:36:31 -0500

    feat: implement BreedPicture component with dynamic breed image loading and localization updates

--- src/app/features/breed-picture/breed-picture.component.ts
+++ src/app/features/breed-picture/breed-picture.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { DogApiService } from '../../core/api/dog-api.service';
+import { ActivatedRoute } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-breed-picture',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './breed-picture.component.html',
-  styleUrl: './breed-picture.component.scss'
+  styleUrl: './breed-picture.component.scss',
 })
-export class BreedPictureComponent {
+export class BreedPictureComponent implements OnInit {
+  private dogApiService = inject(DogApiService);
+  private route = inject(ActivatedRoute);
 
+  public breedName: string = '';
+  public subBreedName: string = '';
+  public breedPicture: string = '';
+
+  ngOnInit() {
+    this.route.params.subscribe(({ breed, subBreed }) => {
+      this.breedName = breed;
+      this.subBreedName = subBreed || '';
+
+      const image$ = this.subBreedName
+        ? this.dogApiService.getRandomImageByBreed(
+            `${this.breedName}/${this.subBreedName}`
+          )
+        : this.dogApiService.getRandomImageByBreed(this.breedName);
+      image$.subscribe((image) => {
+        this.breedPicture = image;
+      });
+    });
+  }
 }
